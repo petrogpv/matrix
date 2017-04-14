0b74e89f0d8ffc8ea5c47374683318e852b0876f
Petro Gordeychuk <petrogpv@mail.ru>
2017-04-14 14:38:49 +0300

    1

--- src/com/petro/matrix/Main.java
+++ src/com/petro/matrix/Main.java
@@ -2,15 +2,21 @@ package com.petro.matrix;
 
 public class Main {
 
+    private static Matrix matrix;
+    private static Matrix matrix2;
+    private int i;
+    private int r;
+
     public static void main(String[] args) {
 
-        Matrix matrix = MatrixFactory.createMatrix(2, 7);
-        Matrix matrix2 = MatrixFactory.createMatrix(2, 7);
+        matrix = MatrixFactory.createMatrix(2, 7);
+        matrix2 = MatrixFactory.createMatrix(2, 7);
 
         System.out.println(matrix.toString());
         System.out.println("+");
         System.out.println(matrix2.toString());
         System.out.println("=");
         System.out.println(Summator.sumMatrices(matrix, matrix2).toString());
+
     }
 }
